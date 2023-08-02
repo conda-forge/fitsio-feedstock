299d2d8eafb83d2fce8857c3a04fff517c93d9cc
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-08-02 15:15:46 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,6 @@
+# configure has a weird choice the ignores LDFLAGS
+export CFLAGS="${CFLAGS} ${LDFLAGS}"
+
 # Get an updated config.sub and config.guess
-# cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./cfitsio-*
-$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv || ls -lah cfitsio-4.2.0/*
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./cfitsio-*
+$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
