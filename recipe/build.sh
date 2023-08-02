cba3f346c31b2110dd02a546a20ff4b310c79926
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-08-02 13:21:51 -0500

    Update recipe/build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,3 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./cfitsio-*
-$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
+$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv || cat cfitsio-*/config.*
