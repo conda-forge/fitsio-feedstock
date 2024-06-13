08030aee120f8e6cc020b51f7f4e88f492c00add
Matthew R. Becker <beckermr@users.noreply.github.com>
2024-06-13 16:47:51 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,6 @@
 export CFLAGS="${CFLAGS} ${LDFLAGS}"
 
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./cfitsio-4.2.0
+cfitsio_dir=$(ls -1d ./cfitsio-*)
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* ${cfitsio_dir}
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
